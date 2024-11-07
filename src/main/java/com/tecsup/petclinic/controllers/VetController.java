e8097388279417f477bf9e3280f8ec510ce04cf6
SVillalva <mstefanovg@gmail.com>
2024-11-07 16:53:06 -0500

    Villalva Funcion

--- src/main/java/com/tecsup/petclinic/controllers/VetController.java
+++ src/main/java/com/tecsup/petclinic/controllers/VetController.java
@@ -1,4 +1,30 @@
+// VetController.java
 package com.tecsup.petclinic.controllers;
 
+import com.tecsup.petclinic.entities.Vet;
+import com.tecsup.petclinic.repositories.VetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/vets")
 public class VetController {
+
+    @Autowired
+    private VetRepository vetRepository;
+
+
+
+    // crea
+    @PostMapping
+    public Vet createVet(@RequestBody Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+
+
 }
--- src/test/java/com/tecsup/petclinic/controllers/VetControllerIntegrationTest.java
+++ src/test/java/com/tecsup/petclinic/controllers/VetControllerIntegrationTest.java
@@ -1,4 +1,42 @@
 package com.tecsup.petclinic.controllers;
 
+import com.tecsup.petclinic.entities.Vet;
+import com.tecsup.petclinic.repositories.VetRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class VetControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private VetRepository vetRepository;
+
+
+
+    @Test
+    public void shouldCreateVet() throws Exception {
+        String newVetJson = "{ \"firstName\": \"Dr. Jane\", \"lastName\": \"Doe\" }";
+
+        mockMvc.perform(post("/api/vets")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newVetJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value("Dr. Jane"))
+                .andExpect(jsonPath("$.lastName").value("Doe"));
+    }
+
+
+
+
 }
